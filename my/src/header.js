f157c2a9e971517edaaf5022de19c433a56df642
chobouser77 <gl8310239@gmail.com>
2024-11-29 15:54:42 +0900

    아직탑바 만드는 중 임시 저장

---
+++ my/src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import './header.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// 기능
+
+function header() {
+  return {
+    <header className="container header">
+
+
+    </header>
+  }
+}
