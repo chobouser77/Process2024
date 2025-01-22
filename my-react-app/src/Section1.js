9fc1172fd7cb6aca612acafe44f41fbd57751bd6
twinogre <glad2you@gmail.com>
2025-01-22 14:26:24 +0900

    약간 수정 / s1 , 라이트 배경

--- my-react-app/src/Section1.js
+++ my-react-app/src/Section1.js
@@ -3,8 +3,6 @@ import "./index.scss";
 import "./Section.scss";
 
 const Section1 = () => {
-
-
     return (
         <div className="section0 section1">
             <p className="big-font yoso">
@@ -18,14 +16,14 @@ const Section1 = () => {
                 </div>
                 <div className="s1-r s1-r2">
                     <p>
-                        2024년 국비과정을 통해 UI / UX 과정을 통해 퍼블리싱에 흥미가 생겨,<br/>
-                        퍼블리셔, 더 나아가 프론트엔드까지 관심이 생겨 react 까지 공부 예정 입니다.
+                        2024년 UI / UX 국비과정을 통해 퍼블리싱에 흥미가 생겨 코딩을 더 공부하게 되었고,<br/>
+                        퍼블리셔, 더 나아가 프론트엔드를 목표로 현재 React 까지 공부 중 입니다.
                     </p>
                     <p> 
                         html , css , js 를 기반으로 퍼블리싱을 진행하고 다양한 프로젝트를 통해<br/>
                         디자이너 , 개발자와 협업 하는 법을 배웠으며 ,<br/>
-                        scass 혹은 다양한 라이브러리를 활용하여 이펙트를 줄 수 있으며,<br/>
-                        다양한 도전을 하고자 합니다.
+                        scss 혹은 다양한 라이브러리를 활용하여 이펙트를 줄 수 있으며,<br/>
+                        그 밖에도 다양한 도전을 하고자 합니다.
                     </p>  
                 </div>
             </div>
